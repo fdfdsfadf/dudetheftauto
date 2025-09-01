2dfef3d7e8f22a298f51af247bf20ad20d68c8eb
fdfdsfadf <Hu_Gaming1@outlook.com>
2025-09-02 08:18:08 +1000

    Create webConfig.js

---
+++ Fetches/webConfig.js
@@ -0,0 +1,8 @@
+{
+  "projectId": "dude-theft-auto-1",
+  "appId": "1:674216240398:web:9326464468eb7102eb235f",
+  "storageBucket": "dude-theft-auto-1.firebasestorage.app",
+  "authDomain": "dude-theft-auto-1.firebaseapp.com",
+  "messagingSenderId": "674216240398",
+  "measurementId": "G-YJZJDS7Y7Y"
+}
